84744d722443f0ade52a240d4db0e506591e8c85
Farzad Farshchi <farzadfr@gmail.com>
2018-04-06 22:13:32 -0500

    Uses old method for getting checkpoint

--- gen-checkpoint.sh
+++ gen-checkpoint.sh
@@ -6,4 +6,4 @@ kernel_path=$(realpath ../gem5-linux/vmlinux)
 dtb_path=$(realpath ../gem5-linux/arch/arm/boot/dts/vexpress-v2p-ca15-tc1-gem5_4cpus.dtb)
 
 rm -rf m5out
-./build/ARM/gem5.opt -d m5out configs/example/fs.py --disk-image=${disk_image_path} --num-cpus=4 --caches --l2cache --mem-size=2048MB --kernel=${kernel_path} --machine-type=VExpress_EMM --dtb-file=${dtb_path} --mem-type=lpddr2_s4_1066_x32 --checkpoint-at-end --script=palloc-chkp.rcS
+./build/ARM/gem5.opt -d m5out configs/example/fs.py --disk-image=${disk_image_path} --num-cpus=4 --caches --l2cache --mem-size=2048MB --kernel=${kernel_path} --machine-type=VExpress_EMM --dtb-file=${dtb_path} --mem-type=lpddr2_s4_1066_x32 --checkpoint-at-end
