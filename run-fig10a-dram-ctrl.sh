fdadace7f69021a6f94b14a5ffc48fda0b61c9b6
Farzad Farshchi <farzadfr@gmail.com>
2018-05-23 01:06:10 -0500

    run scripts for fig 9 and 10a can limit the number of simulations to luanch

--- run-fig10a-dram-ctrl.sh
+++ run-fig10a-dram-ctrl.sh
@@ -2,11 +2,12 @@
 
 declare -a arr=("disparity-cif-solo" "mser-cif-solo" "sift-cif-solo" "svm-cif-solo" "texture_synthesis-cif-solo" "disparity-cif-3b4096-budfr" "mser-cif-3b4096-budfr" "sift-cif-3b4096-budfr" "svm-cif-3b4096-budfr" "texture_synthesis-cif-3b4096-budfr" "disparity-cif-3b4096-mdu" "mser-cif-3b4096-mdu" "sift-cif-3b4096-mdu" "svm-cif-3b4096-mdu" "texture_synthesis-cif-3b4096-mdu" "disparity-cif-3b4096-dma" "mser-cif-3b4096-dma" "sift-cif-3b4096-dma" "svm-cif-3b4096-dma" "texture_synthesis-cif-3b4096-dma" "disparity-cif-3b4096-dmt98" "mser-cif-3b4096-dmt98" "sift-cif-3b4096-dmt98" "svm-cif-3b4096-dmt98" "texture_synthesis-cif-3b4096-dmt98" "disparity-cif-3b4096-dmt90" "mser-cif-3b4096-dmt90" "sift-cif-3b4096-dmt90" "svm-cif-3b4096-dmt90" "texture_synthesis-cif-3b4096-dmt90")
 
-if [ "$#" -ne 1 ]; then
-    echo "Error: please specify the checkpoint path."
+if [ "$#" -ne 2 ]; then
+    echo "Error: please specify the checkpoint path and the number of simulations to be run simultaneously."
     exit
 fi
 chkp_path=$1
+n_sim=$2
 
 export M5_PATH=$(realpath ../full_system_images)
 disk_image_path=$(realpath ../full_system_images/disks/linux-arm-ael.img)
@@ -14,10 +15,18 @@ kernel_path=$(realpath ../gem5-linux/vmlinux)
 
 mkdir -p ../results/fig10a-dram-ctrl/stdout
 
-for i in "${arr[@]}"
+j="0"
+
+while [ $j -lt 48 ]
 do
+     i="${arr[$j]}"
      ./build/ARM/gem5.opt -d ../results/fig10a-dram-ctrl --stats-file=$i.txt configs/spec2006/simpoint_fs.py --cfg=configs/spec2006/arm.cfg --disk-image=${disk_image_path} --num-cpus=4 --mem-size=2048MB --kernel=${kernel_path} --machine-type=VExpress_EMM --mem-type=lpddr2_s4_1066_x32 --simpoint-mode=batch --benchmark=$i --checkpoint-dir=${chkp_path} 2>&1 | tee ../results/fig10a-dram-ctrl/stdout/$i.txt  &
+     j=$[$j+1]
+     if [ "$[$j%$n_sim]" -eq 0 ]; then
+         wait
+     fi
 done
+
 
 wait
 echo === run-fig10a-dram-ctrl.sh is finished ===
--- run-fig9-be-effect.sh
+++ run-fig9-be-effect.sh
@@ -2,11 +2,12 @@
 
 declare -a arr=("3disparity-inf-1bzip2-ei-dmt98" "3mser-inf-1bzip2-ei-dmt98" "3sift-inf-1bzip2-ei-dmt98" "3svm-inf-1bzip2-ei-dmt98" "3texture_synthesis-inf-1bzip2-ei-dmt98" "3aifftr01-inf-1bzip2-ei-dmt98" "3aiifft01-inf-1bzip2-ei-dmt98" "3matrix01-inf-1bzip2-ei-dmt98" "3disparity-inf-1bzip2-ei-dmt90" "3mser-inf-1bzip2-ei-dmt90" "3sift-inf-1bzip2-ei-dmt90" "3svm-inf-1bzip2-ei-dmt90" "3texture_synthesis-inf-1bzip2-ei-dmt90" "3aifftr01-inf-1bzip2-ei-dmt90" "3aiifft01-inf-1bzip2-ei-dmt90" "3matrix01-inf-1bzip2-ei-dmt90" "3disparity-inf-1bzip2-ei-dma" "3mser-inf-1bzip2-ei-dma" "3sift-inf-1bzip2-ei-dma" "3svm-inf-1bzip2-ei-dma" "3texture_synthesis-inf-1bzip2-ei-dma" "3aifftr01-inf-1bzip2-ei-dma" "3aiifft01-inf-1bzip2-ei-dma" "3matrix01-inf-1bzip2-ei-dma" "3disparity-inf-1bzip2-ei-dmh" "3mser-inf-1bzip2-ei-dmh" "3sift-inf-1bzip2-ei-dmh" "3svm-inf-1bzip2-ei-dmh" "3texture_synthesis-inf-1bzip2-ei-dmh" "3aifftr01-inf-1bzip2-ei-dmh" "3aiifft01-inf-1bzip2-ei-dmh" "3matrix01-inf-1bzip2-ei-dmh" "3disparity-inf-1bzip2-ei-wp" "3mser-inf-1bzip2-ei-wp" "3sift-inf-1bzip2-ei-wp" "3svm-inf-1bzip2-ei-wp" "3texture_synthesis-inf-1bzip2-ei-wp" "3aifftr01-inf-1bzip2-ei-wp" "3aiifft01-inf-1bzip2-ei-wp" "3matrix01-inf-1bzip2-ei-wp" "3disparity-inf-1bzip2-ei-nop" "3mser-inf-1bzip2-ei-nop" "3sift-inf-1bzip2-ei-nop" "3svm-inf-1bzip2-ei-nop" "3texture_synthesis-inf-1bzip2-ei-nop" "3aifftr01-inf-1bzip2-ei-nop" "3aiifft01-inf-1bzip2-ei-nop" "3matrix01-inf-1bzip2-ei-nop")
 
-if [ "$#" -ne 1 ]; then
-    echo "Error: please specify the checkpoint path."
+if [ "$#" -ne 2 ]; then
+    echo "Error: please specify the checkpoint path and the number of simulations to be run simultaneously."
     exit
 fi
 chkp_path=$1
+n_sim=$2
 
 export M5_PATH=$(realpath ../full_system_images)
 disk_image_path=$(realpath ../full_system_images/disks/linux-arm-ael.img)
@@ -14,9 +15,16 @@ kernel_path=$(realpath ../gem5-linux/vmlinux)
 
 mkdir -p ../results/fig9-be-effect/stdout
 
-for i in "${arr[@]}"
+j="0"
+
+while [ $j -lt 48 ]
 do
+     i="${arr[$j]}"
      ./build/ARM/gem5.opt -d ../results/fig9-be-effect --stats-file=$i.txt configs/spec2006/simpoint_fs.py --cfg=configs/spec2006/arm.cfg --disk-image=${disk_image_path} --num-cpus=4 --mem-size=2048MB --kernel=${kernel_path} --machine-type=VExpress_EMM --mem-type=lpddr2_s4_1066_x32 --simpoint-mode=batch --benchmark=$i --checkpoint-dir=${chkp_path} 2>&1 | tee ../results/fig9-be-effect/stdout/$i.txt &
+     j=$[$j+1]
+     if [ "$[$j%$n_sim]" -eq 0 ]; then
+         wait
+     fi
 done
 
 wait
